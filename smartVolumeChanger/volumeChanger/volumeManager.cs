85927ea457aa96f38898ae8ea0e173e9e4f3542b
Cyp[Lee Byoung Gyu] <cypher9715@naver.com>
2021-08-01 23:07:14 +0900

    초기 커밋.

---
+++ smartVolumeChanger/volumeChanger/volumeManager.cs
@@ -0,0 +1,46 @@
+﻿using CSCore.CoreAudioAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartVolumeChanger
+{
+    class volumeChanger
+    {
+        public void init()
+        {
+            using (AudioSessionManager2 sessionManager = GetDefaultAudioSessionManager2(DataFlow.Render))
+            {
+                using (var sessionEnumerator = sessionManager.GetSessionEnumerator())
+                {
+                    SortedDictionary<int, string> sortedDic = new SortedDictionary<int, string>();
+
+                    foreach (var session in sessionEnumerator)
+                    {
+                        using (var simpleVolume = session.QueryInterface<SimpleAudioVolume>())
+                        using (var sessionControl = session.QueryInterface<AudioSessionControl2>())
+                        {
+                            sortedDic.Add(sessionControl.ProcessID, sessionControl.Process.ProcessName);
+                            Console.WriteLine(sessionControl.Process.ProcessName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static AudioSessionManager2 GetDefaultAudioSessionManager2(DataFlow dataFlow)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    var sessionManager = AudioSessionManager2.FromMMDevice(device);
+                    return sessionManager;
+                }
+            }
+        }
+    }
+
+}
